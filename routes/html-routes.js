ee1626315750bd7ed86fd029f8bf21c033ee8bd5
Erin Hagerty <erinhagerty@Erins-MBP.attlocal.net>
2017-12-19 21:31:24 -0800

    housedivs & leaderboard

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -24,13 +24,9 @@ module.exports = function (app) {
 
   //NEED TO EDIT CODE TO REFLECT A USER SEPCIFIC COLLECTION ONCE WE DO THAT LOGIC//
   app.get("/collection", function (req, res) {
-    
-
     //need to figure out this @#O)OEFH)@ cookie
     console.log("this is backend cookie"+req.cookies.identity);
-
     console.log("this worked");
-
     var id;
 
     db.User.findOne({
@@ -44,7 +40,6 @@ module.exports = function (app) {
         console.log("this is ID: "+id);
 
         db.User.findAll({
-
           include:[
             {
               model:db.Item,
@@ -56,17 +51,88 @@ module.exports = function (app) {
             }
 
           }).then(function (data) {
-            console.log("this is Item"+data[0].Items);
+            console.log("this is Item" + JSON.stringify(data[0].Items));
+            var stark = [];
+            var lannister = [];
+            var targaryen = [];
+            var baratheon = [];
+            var martell = [];
+            var tyrell = [];
+            var wildlings = [];
+            var nightswatch = [];
+            var warriors = [];
+            var smallCouncil = [];
+            var other = [];
+            var cat = [];
+            var direwolves = [];
+
+            for (var i = 0; i < data[0].Items.length; i++) {
+              if (data[0].Items[i].house === "Stark"){
+                stark.push(data[0].Items[i]);
+              }
+              else if (data[0].Items[i].house === "Lannister"){
+                lannister.push(data[0].Items[i]);
+              }
+              else if (data[0].Items[i].house === "Targaryen"){
+                targaryen.push(data[0].Items[i]);
+              }
+              else if (data[0].Items[i].house === "Baratheon"){
+                baratheon.push(data[0].Items[i]);
+              }
+              else if (data[0].Items[i].house === "Martell"){
+                martell.push(data[0].Items[i]);
+              }
+              else if (data[0].Items[i].house === "Tyrell"){
+                tyrell.push(data[0].Items[i]);
+              }
+              else if (data[0].Items[i].house === "Wildlings"){
+                wildlings.push(data[0].Items[i]);
+              }
+              else if (data[0].Items[i].house === "Nightswatch"){
+                nightswatch.push(data[0].Items[i]);
+              }
+              else if (data[0].Items[i].house === "Warriors of Westeros"){
+                warriors.push(data[0].Items[i]);
+              }
+              else if (data[0].Items[i].house === "Small Council"){
+                smallCouncil.push(data[0].Items[i]);
+              }
+              else if (data[0].Items[i].house === "Other"){
+                other.push(data[0].Items[i]);
+              }
+              else if (data[0].Items[i].house === "Direwolves"){
+                direwolves.push(data[0].Items[i]);
+              }
+              else if (data[0].Items[i].house === "Cat"){
+                cat.push(data[0].Items[i]);
+              }
+
+            }
             var hbsObject = {
               // kitty: data.Item
-              kitty: data[0].Items
-            }; 
+              kitty: {stark: stark,
+                 lannister:lannister,
+                 baratheon:baratheon,
+                 targaryen:targaryen,
+                 warriors: warriors,
+                 direwolves:direwolves,
+                 cat:cat,
+                 martell:martell,
+                 tyrell:tyrell,
+                 targaryen:targaryen,
+                 nightswatch:nightswatch,
+                 other:other,
+                 smallCouncil:smallCouncil,
+                 wildlings:wildlings
+                      }
+            };
+            //console.log(hbsObject);
             res.render("collection", hbsObject);
           });
 
     });
 
-    
+
 
   });
 
@@ -76,7 +142,7 @@ module.exports = function (app) {
     // app.get("/api/collection/:id",function(req,res){
 
     // console.log(req.params.id);
-        
+
     //     db.User.findAll({
     //     // attributes:["id"],
     //       include:[
@@ -88,7 +154,7 @@ module.exports = function (app) {
     //       ],
     //       where:{id:req.params.id}
     //     }).then(function(data){
-          
+
     //       res.json(data);
     //   });
 
@@ -99,12 +165,12 @@ module.exports = function (app) {
     app.get("/login-page", function(req, res){
         res.render("login-page");
     });
-    
+
     app.get("/admin", function (req, res) {
       db.Item.findAll({}).then(function (data) {
         var hbsObject = {
           kitty: data
-        }; 
+        };
         res.render("admin-cats", hbsObject);
       });
     });
@@ -113,7 +179,7 @@ module.exports = function (app) {
       db.User.findAll({}).then(function (data) {
         var hbsObject = {
           leaders: data
-        }; 
+        };
         res.json(hbsObject);
       });
     });
@@ -124,8 +190,7 @@ module.exports = function (app) {
   //   db.User.findOne({}).then(function (data) {
   //     var coinCount = {
   //       coins: data.coin_count
-  //     }; 
+  //     };
   //     res.render("/*", coinCount);
   //   });
   // });
-
